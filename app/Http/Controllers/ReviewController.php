eb2198963e7063140101f1f3585ae2a75b04782e
Nsk04 <s.nomura@gmail.com>
2024-09-24 00:21:39 +0900

    レビューの編集

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -17,8 +17,7 @@ class ReviewController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'content' => 'required',
-            'restaurant_id' => 'required|exists:restaurants,id' // ここで必須かつ存在するrestaurant_idであることを確認
+            'content' => 'required'
     ]);
 
         $review = new Review();
