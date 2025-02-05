033f71368dc84c42485c778f37cacbca41cb4149
Nsk04 <s.nomura@gmail.com>
2025-02-05 23:07:01 +0900

    予約画面定休日、予約時間実装中

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -3,40 +3,40 @@ import { Japanese } from "flatpickr/dist/l10n/ja.js"
 import 'flatpickr/dist/flatpickr.css';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const closedDays = '{{$closedDays}}';
+    const closedDays = '{{$closedDays}}';
 
-  console.log(closedDays)
+    console.log(closedDays)
 
-  flatpickr("#reservation_date", {
-      enableTime: false,
-      dateFormat: "Y-m-d",
-      locale: Japanese,
-      minDate: new Date().fp_incr(1),
-      disable: [
-          function(date) {
-              const dayOfWeek = date.getDay();
-              return closedDays.includes(dayOfWeek);
-          }
-      ],
-      onChange: function(selectedDates, dateStr) {
-          updateTimePicker(dateStr);
-      }
-  });
+    flatpickr("#reservation_date", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        locale: Japanese,
+        minDate: new Date().fp_incr(1),
+        disable: [
+            function(date) {
+                const dayOfWeek = date.getDay();
+                return closedDays.includes(dayOfWeek);
+            }
+        ],
+        onChange: function(selectedDates, dateStr) {
+            updateTimePicker(dateStr);
+        }
+    });
 
-  function updateTimePicker(selectedDate) {
-  const minTime = "{{ $minReservationTime }}";
-  const maxTime = "{{ $maxReservationTime }}";
+    function updateTimePicker(selectedDate) {
+    const minTime = "{{ $minReservationTime }}";
+    const maxTime = "{{ $maxReservationTime }}";
 
-  flatpickr("#reservation_time", {
-      enableTime: true, 
-      noCalendar: true, 
-      dateFormat: "H:i",
-      time_24hr: true,
-      minuteIncrement: 30,
-      minTime: "{{ $minReservationTime }}",
-      maxTime: new Date(new Date().setHours(closeHour - 2, closeMinute, 0)), 
-      });
-  }
-  updateTimePicker();
-});
+    flatpickr("#reservation_time", {
+        enableTime: true, 
+        noCalendar: true, 
+        dateFormat: "H:i",
+        time_24hr: true,
+        minuteIncrement: 30,
+        minTime: "{{ $minReservationTime }}",
+        maxTime: new Date(new Date().setHours(closeHour - 2, closeMinute, 0)), 
+        });
+    }
+    updateTimePicker();
+    });
 
